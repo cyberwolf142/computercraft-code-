43ccd448cb24c8afd3a5eaadfec22801bb3f137e
cyberwolf142 <140569026+cyberwolf142@users.noreply.github.com>
2024-01-30 12:50:27 -0500

    Update minev5.lua

--- minev5.lua
+++ minev5.lua
@@ -1,24 +1,17 @@
 local robot = require("robot")
 
-local digDepth = 2 -- adjust based on desired depth
-local areaSide = 10
+local width = 10
+local lenth = 10
 
-function digAndClear(digDepth, clearLength)
-  for i = 1, digDepth do
-    robot.digDown()
-  end
-  for i = 1, clearLength do
-    robot.digForward()
-  end
-end
 
-for i = 1, areaSide do
-  digAndClear(digDepth, areaSide)
-  robot.turnRight()
-  robot.forward()
-end
+for i = 1, width do
+    robot.swing()
+    robot.forward()
+    for i = 1 , lenth do 
+        robot.turnright()
+        robot.swing()
+        robot.forward()
+        robot.turnright()
+    end
 
-robot.turnLeft()
-robot.forward()
 
-print("Finished digging the 10x10 area!")
